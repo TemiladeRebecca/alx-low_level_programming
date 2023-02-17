a12b2d474649da143f0f6fb8acab4b4a4e6be955
TemiladeRebecca <temilade.rebecca20@gmail.com>
2023-02-16 18:20:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-
+#include<unistd.h>
 /**
- * main - prints "and that piece of art is useful"
- * Return: 1
- * 
+ * main - is entry point
+ *
+ * Return: 1, if successful
  */
-
 int main(void)
-
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
