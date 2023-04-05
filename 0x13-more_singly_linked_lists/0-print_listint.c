2feb0daff977dc7d35817b722968516274247f7b
TemiladeRebecca <temilade.rebecca20@gmail.com>
2023-04-05 03:25:22 -0700

    C More singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
     {
-        printf("%d\n", h->n);
-        h = h->n;
         nodes++;
+        printf("%d\n", h->n);
+        h = h->next
     }
     return (nodes);
 }
